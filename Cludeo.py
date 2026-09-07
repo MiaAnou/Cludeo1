a9d61c44721263aa600895b273963060661740c3
MiaAnou <121834126+MiaAnou@users.noreply.github.com>
2026-09-07 09:46:22 +0000

    Create random number from 0 to 5

--- Cludeo.py
+++ Cludeo.py
@@ -1,3 +1,6 @@
+import random
 characters = ("Professor Plum", "Mrs White","Mr Green", "Mrs Peacock", "Miss Scarlett", "Colonel Mustard")
 weapons = ("wrench", "candlestick","lead pipe", "rope", "revolver", "knife")
 rooms = ("Study", "Kitchen","Hall", "Conservatory", "Lounge", "Ballroom", "Dining Room", "Libary", "Billiard Room")
+
+random_num = rand_int(0,5)

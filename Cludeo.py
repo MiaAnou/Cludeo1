72e17c6b9305b3fc547cdf17a398f1c514338e60
MiaAnou <121834126+MiaAnou@users.noreply.github.com>
2026-09-07 09:39:13 +0000

    Create rooms list

--- Cludeo.py
+++ Cludeo.py
@@ -1,2 +1,3 @@
 characters = ("Professor Plum", "Mrs White","Mr Green", "Mrs Peacock", "Miss Scarlett", "Colonel Mustard")
 weapons = ("wrench", "candlestick","lead pipe", "rope", "revolver", "knife")
+rooms = ("Study", "Kitchen","Hall", "Conservatory", "Lounge", "Ballroom", "Dining Room", "Libary", "Billiard Room")

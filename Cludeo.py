339adff551d0f7c225c78908fc37d3135b66ef27
MiaAnou <121834126+MiaAnou@users.noreply.github.com>
2026-09-07 09:33:33 +0000

    Create weapons list

--- Cludeo.py
+++ Cludeo.py
@@ -1,1 +1,2 @@
 characters = ("Professor Plum", "Mrs White","Mr Green", "Mrs Peacock", "Miss Scarlett", "Colonel Mustard")
+weapons = ("wrench", "candlestick","lead pipe", "rope", "revolver", "knife")

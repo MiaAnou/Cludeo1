7821e79e25a2b388d86e2cc26b7d9e7c92abd87c
MiaAnou <121834126+MiaAnou@users.noreply.github.com>
2026-09-08 08:41:15 +0000

    Get murder room & weapon

--- Cludeo.py
+++ Cludeo.py
@@ -3,8 +3,7 @@ characters = ("Professor Plum", "Mrs White","Mr Green", "Mrs Peacock", "Miss Sca
 weapons = ("wrench", "candlestick","lead pipe", "rope", "revolver", "knife")
 rooms = ("Study", "Kitchen","Hall", "Conservatory", "Lounge", "Ballroom", "Dining Room", "Libary", "Billiard Room")
 
-character_mur = random.randint(0,5)
-room_mur = random.randint(0,5)
-weapon_mur = random.randint(0,5)
+murderer = characters[random.randint(0,5)]
+murder_room = rooms[random.randint(0,5)]
+murder_weapon = weapons[random.randint(0,5)]
 
-murderer = characters[character_mur]

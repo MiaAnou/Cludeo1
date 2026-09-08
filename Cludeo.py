22e3b2672f7b6ab9ca770dfdfeef691edf8c5ae5
MiaAnou <121834126+MiaAnou@users.noreply.github.com>
2026-09-08 09:47:09 +0000

    Create procedure to assess guess

--- Cludeo.py
+++ Cludeo.py
@@ -3,12 +3,26 @@ characters = ("Professor Plum", "Mrs White","Mr Green", "Mrs Peacock", "Miss Sca
 weapons = ("wrench", "candlestick","lead pipe", "rope", "revolver", "knife")
 rooms = ("Study", "Kitchen","Hall", "Conservatory", "Lounge", "Ballroom", "Dining Room", "Libary", "Billiard Room")
 
-def get_info():
+def get_info(): #procedure to get info for the game
     murderer = characters[random.randint(0,5)]
     murder_room = rooms[random.randint(0,5)]
     murder_weapon = weapons[random.randint(0,5)]
 
-    print(murderer)
-    print(murder_room)
-    print(murder_weapon)
 
+def assess_guess():
+    count = 0
+    character_guess = input().lower()
+    weapon_guess = input().lower()    
+    room_guess = input().lower()
+
+    if character_guess == murderer:
+        count = count + 1
+    if weapon_guess == murder_weapon:
+        count = count + 1
+    if room_guess == murder_room:
+        count = count + 1
+
+    print(count, " guesses correct")
+
+get_info()
+assess_guess()

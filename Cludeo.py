e5b55620cca4d9b6ae8de95118af5555333d1652
MiaAnou <121834126+MiaAnou@users.noreply.github.com>
2026-09-08 08:44:08 +0000

    Create procedure to get info

--- Cludeo.py
+++ Cludeo.py
@@ -3,7 +3,8 @@ characters = ("Professor Plum", "Mrs White","Mr Green", "Mrs Peacock", "Miss Sca
 weapons = ("wrench", "candlestick","lead pipe", "rope", "revolver", "knife")
 rooms = ("Study", "Kitchen","Hall", "Conservatory", "Lounge", "Ballroom", "Dining Room", "Libary", "Billiard Room")
 
-murderer = characters[random.randint(0,5)]
-murder_room = rooms[random.randint(0,5)]
-murder_weapon = weapons[random.randint(0,5)]
+def get_info():
+    murderer = characters[random.randint(0,5)]
+    murder_room = rooms[random.randint(0,5)]
+    murder_weapon = weapons[random.randint(0,5)]
 
